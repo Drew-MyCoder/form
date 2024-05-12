23923891d5e0c0d6d179ef508ebd57673cdd5a06
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-05-12 15:24:36 +0000

    sets up the app and routing

--- form/src/App.jsx
+++ form/src/App.jsx
@@ -1,18 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 import { Register } from './Register'
 import Login from './Login'
+import Layout from './components/Layout'
+import { Route, Routes } from 'react-router-dom'
+import Editor from './components/Editor'
+import Admin from './components/Admin'
+import Lounge from './components/Lounge'
+import Missing from './components/Missing'
+import Home from './components/Home'
+import Unauthorized from './components/Unauthorized'
+import LinkPage from './components/LinkPage'
+import RequireAuth from './components/RequiredAuth'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
-    <main className='App'>
-      <Login />  
-    </main>
-  )
+    <>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        {/* Public roytes */}
+        <Route path='login' element={<Login />} />
+        <Route path='register' element={<Register />} />
+        <Route path='linkpage' element={<LinkPage />} />
+        <Route path='unauthorized' element={<Unauthorized />} />
+
+        {/* we want to protect these routes, allowed roles not implemented */}
+        <Route element={<RequireAuth allowedRoles={[2001]}/>} >
+        <Route path='/' element={<Home />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[1984]}/>} >
+        <Route path='editor' element={<Editor />} />
+        </Route>
+
+        <Route element={<RequireAuth/>} >
+        <Route path='admin' element={<Admin />} />
+        </Route>
+
+        <Route element={<RequireAuth allowedRoles={[1984, 5150]}/>} >
+        <Route path='lounge' element={<Lounge />} />
+        </Route>
+
+        {/* catch all */}
+        <Route path='*' element={<Missing />} />
+      </Route>
+    </Routes>
+    </>
+  );
 }
 
 export default App
